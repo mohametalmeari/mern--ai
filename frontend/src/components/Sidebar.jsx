289a75e3102bf380f4612b276c5a8b62e6fb452a
mohametalmeari <mohamet.almeari@gmail.com>
2024-10-09 17:32:41 +0300

    Create dashboard page - Display a list of links for different sections

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -9,32 +9,42 @@ import { IoClose, IoSettingsOutline } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import { closeMenu } from "../redux/features/menu/menuSlice";
 
-const Links = [
-  { name: "Dashboard", path: "/dashboard", icon: <RxDashboard /> },
+export const Links = [
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: <RxDashboard />,
+    color: "#2196F3",
+  },
   {
     name: "Conversation",
     path: "/dashboard/conversation",
     icon: <BiConversation />,
+    color: "#FFC107",
   },
   {
     name: "Image Generation",
     path: "/dashboard/image-generation",
     icon: <FaRegImage />,
+    color: "#FF5722",
   },
   {
     name: "Video Generation",
     path: "/dashboard/video-generation",
     icon: <LuVideo />,
+    color: "#03A9F4",
   },
   {
     name: "Music Generation",
     path: "/dashboard/music-generation",
     icon: <FiMusic />,
+    color: "#4CAF50",
   },
   {
     name: "Code Generation",
     path: "/dashboard/code-generation",
     icon: <IoMdCode />,
+    color: "#9C27B0",
   },
   {
     name: "Settings",
@@ -67,7 +77,7 @@ export const Sidebar = () => {
         {Links.map((l) => (
           <li key={l.path} className="sidebar-menu-item">
             <Link to={l.path} className={path === l.path ? "active" : ""}>
-              {l.icon}
+              <span style={{ color: l.color }}>{l.icon}</span>
               {l.name}
             </Link>
           </li>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom";
+import { Links } from "../components";
+import { FaArrowRight } from "react-icons/fa6";
+
 export const Dashboard = () => {
-  return <main>Dashboard</main>;
+  return (
+    <main className="dashboard-page">
+      <div className="dashboard-header">
+        <h1>Explore the power of AI</h1>
+        <h2>Chat with the smartest AI - Experience the power of AI</h2>
+      </div>
+      <ul className="dashboard-links">
+        {Links.slice(1, 6).map((l) => (
+          <li key={l.path}>
+            <Link to={l.path} className="dashboard-link">
+              <div
+                className="dashboard-link-icon"
+                style={{ color: l.color, background: `${l.color}22` }}
+              >
+                {l.icon}
+              </div>
+              <div className="dashboard-link-name">{l.name}</div>
+              <FaArrowRight />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 };
